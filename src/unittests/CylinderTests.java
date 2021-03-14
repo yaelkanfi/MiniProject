af89b162fdb0f8ec5cfb268cee538f216206c204
yaelkanfi01@gmail.com <yaelkanfi01@gmail.com>
2021-03-14 20:50:11 +0200

    cylinder

--- src/unittests/CylinderTests.java
+++ src/unittests/CylinderTests.java
@@ -24,10 +24,19 @@ public class CylinderTests {
 	 */
 	@Test
 	public void testGet_Normal() {
-		// ============ Equivalence Partitions Tests ==============
-			  Ray r = new Ray(new Point3D(1,0,0), new Vector(0,1,0));
-			  Cylinder cyl = new Cylinder(r, 1, 1);
-			  assertEquals("Wrong normal to Cylinder", new Vector(1, 0, 0), cyl.get_Normal(new Point3D(2, 0, 0)));
+		// ============ Equivalence Partitions Tests =================
+			 Cylinder c = new Cylinder(new Ray(new Point3D(0, 0, 0), new Vector(0, 0, 1)), 3, 10);
+			 Vector v = new Vector(0, 0, -1);
+			 assertEquals("Bad normal to cylinder", v, c.get_Normal(new Point3D(2, 2, 0)));
+			 v = new Vector(0, 0, 1);
+			 assertEquals("Bad normal to cylinder", v, c.get_Normal(new Point3D(1, 2, 10)));
+			 v = new Vector(1, 0, 0);
+	        assertEquals("Bad normal to cylinder", v, c.get_Normal(new Point3D(3, 0, 6)));
+	     // ============ Boundary Values Tests =========================
+	        v = new Vector(1, 0, 0);
+	        assertEquals("Bad normal to cylinder", v, c.get_Normal(new Point3D(3, 0, 0)));
+	        v = new Vector(1, 0, 0);
+	        assertEquals("Bad normal to cylinder", v, c.get_Normal(new Point3D(3, 0, 10)));
 	}
 
 }

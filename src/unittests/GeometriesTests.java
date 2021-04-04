637aadc1652c9390eac139446c51641a8dd9f74c
yaelkanfi01@gmail.com <yaelkanfi01@gmail.com>
2021-04-04 21:37:45 +0300

    targil3 finished

--- src/unittests/GeometriesTests.java
+++ src/unittests/GeometriesTests.java
@@ -33,34 +33,34 @@ public class GeometriesTests {
 		Geometries empty = new Geometries(); //empty list
 		Geometries geometries = new Geometries();
 		//add geometries to list
-		geometries.add(new Sphere(new Point3D(10, 0, 0), 1), //new sphere
-				new Triangle(new Point3D(1, 0, 0), new Point3D(1, 1, -1), new Point3D(1, -1, -1)), //new triangle
-				new Plane(new Point3D(-5, 0, 0), new Vector(1, 0, 0))); //new plane
+		geometries.add(new Sphere(new Point3D(4,0,0), 1d), //new sphere
+				new Triangle(new Point3D(2,0,1),new Point3D(2,0,-1),new Point3D(2,1,0)), //new triangle
+				new Plane(new Point3D(0,0,0), new Vector(1,0,0))); //new plane
 		
 		// ============ Equivalence Partitions Tests ==============
 		
 		//TC01: some geometries are intersected but not all of them
-		Ray ray = new Ray(new Point3D(0.5, 0 ,0), new Vector(1, 0, 0));
+		Ray ray = new Ray(new Point3D(-1,2,0), new Vector(1, 0, 0));
 		List<Point3D> result = geometries.findIntersections(ray);
-		assertEquals("Wrong number of points", 2, result.size());
+		assertEquals("Wrong number of points", 1, result.size());
 		
 		// =============== Boundary Values Tests ==================
 		
 		//TC11: empty collection
-	    ray = new Ray(new Point3D(0, 0, 2), new Vector(0, 0, 0.5));
+	    ray = new Ray(new Point3D(1,1,0), new Vector(1, 0, 0));
 		assertNull("Geometries is empty", empty.findIntersections(ray));
 		
 		//TC12: no geometry is intersected
-		ray = new Ray(new Point3D(0, 5, 0), new Vector(1, 1, 2));
+		ray = new Ray(new Point3D(-1,0,0), new Vector(0,1, 0));
 		assertNull("Geometries intersected", geometries.findIntersections(ray));
 		
 		//TC13: one geometry is intersected
-		ray = new Ray(new Point3D(-4, 0 ,0), new Vector(-1, 0, 0));
+		ray = new Ray(new Point3D(2.5,0,0), new Vector(4,0,0));
 	    result = geometries.findIntersections(ray);
-		assertEquals("Wrong number of points", 1, result.size());
+		assertEquals("Wrong number of points", 2, result.size());
 		
 		//TC14: all geometries are intersected
-		ray = new Ray(new Point3D(-6, 0 ,0), new Vector(1, 0, 0));
+		ray = new Ray(new Point3D(-1,0,0), new Vector(1,0,0));
 		result = geometries.findIntersections(ray);
 		assertEquals("Wrong number of points", 3, result.size());
 	}

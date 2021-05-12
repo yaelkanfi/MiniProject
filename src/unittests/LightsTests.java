f244fb3b08f3a73ba5ccab510dded58955f0b8bf
yaelkanfi01@gmail.com <yaelkanfi01@gmail.com>
2021-05-12 18:48:14 +0300

    targil6_final

--- src/unittests/LightsTests.java
+++ src/unittests/LightsTests.java
@@ -140,8 +140,52 @@ public class LightsTests {
 		render.renderImage();
 		render.writeToImage();
 	}
-
 	
+	/**
+	 * Produce a picture of a sphere lighted by all types of lights
+	 */
+	@Test
+	public void sphereAllLights() {
+		scene1.geometries.add(sphere);
+		//add all types of light source
+		scene1.lights.add(new DirectionalLight(new Color(500, 300, 0), new Vector(-1, -1, -1))); //add directional light
+		scene1.lights.add(new PointLight(new Color(500, 300, 0), new Point3D(50, 50, 50), 1, 0.01, 0.00000001)//add point light
+				.setkL(0.01).setkQ(0.00000001));
+		scene1.lights.add(new SpotLight(new Color(500, 300, 0), new Point3D(30, 30, 50), 1, 0.01, 0.00000001, new Vector(3, 1, 0)) //add spot light
+				.setkL(0.01).setkQ(0.00000001));
+		ImageWriter imageWriter = new ImageWriter("SphereAllLights", 500, 500);
+		Render render = new Render()
+				.setImageWriter(imageWriter) 
+				.setCamera(camera1) 
+				.setRayTracer(new RayTracerBasic(scene1));
+		render.renderImage();
+		render.writeToImage();
+	}
+
+	/**
+	 * Produce a picture of a two triangles lighted by all types of lights
+	 */
+	@Test
+	public void trianglesAllLights() {
+		scene2.geometries.add(triangle1.setMaterial(new Material().setkD(0.5).setkS(0.5).setnShininess(300)),
+				triangle2.setMaterial(new Material().setkD(0.5).setkS(0.5).setnShininess(300)));
+		//add all types of light source
+		scene2.lights.add(new DirectionalLight(new Color(200, 150, 150), new Vector(0, 0, 20))); //add directional light
+		scene2.lights.add(new PointLight(new Color(500, 450, 250), new Point3D(10, 10, 130), 1, 0.5, 0.000005) //add point light
+				.setkL(0.5).setkQ(0.000005));
+		scene2.lights.add(new SpotLight(new Color(350, 250, 700), new Point3D(10, -10, -130), 1, 0.001, 0.0005, new Vector(2, -2, -1)) //add spot light
+				.setkL(0.001).setkQ(0.0005));
+
+		ImageWriter imageWriter = new ImageWriter("TrianglesAllLights", 500, 500);
+		Render render = new Render()//
+				.setImageWriter(imageWriter) //
+				.setCamera(camera2) //
+				.setRayTracer(new RayTracerBasic(scene2));
+		render.renderImage();
+		render.writeToImage();
+	}
+
+	/*
 	@Test
 	public void sphereSpotSharp() {
 		scene1.geometries.add(sphere);
@@ -155,11 +199,12 @@ public class LightsTests {
 				.setRayTracer(new RayTracerBasic(scene1));
 		render.renderImage();
 		render.writeToImage();
-	}
+	}*/
 
 	/**
 	 * Produce a picture of a two triangles lighted by a narrow spot light
 	 */
+	/*
 	@Test
 	public void trianglesSpotSharp() {
 		scene2.geometries.add(triangle1.setMaterial(new Material().setkD(0.5).setkS(0.5).setnShininess(300)),
@@ -174,6 +219,6 @@ public class LightsTests {
 				.setRayTracer(new RayTracerBasic(scene2));
 		render.renderImage();
 		render.writeToImage();
-	}
+	}*/
 
 }

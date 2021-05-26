1bf8d05ab9c2f4c569e8b7c8f620dfbb4f40621c
Chagit Shaviv <chagit9555@gmail.com>
2021-05-26 03:17:30 +0300

    commitTest

--- src/unittests/ReflectionRefractionTests.java
+++ src/unittests/ReflectionRefractionTests.java
@@ -126,91 +126,115 @@ public class ReflectionRefractionTests {
 	 *  
 	 */
 	
-	@Test
-	public void threeSpheres() {
-		//Scene scene = new Scene("Test scene");
-		Camera camera =new Camera(new Point3D(0, 0,0), new Vector(0, 0, -1), new Vector(0, -1, 0)).setViewPlaneSize(200, 200).setDistance(200);
-		//scene.setBackground(Color.BLACK);
-		scene.setAmbientLight(new AmbientLight(new Color(20,20,20), 0.0));
+	
+	@Test// Chagit&Yael in the test we made teddy bear that reflected on floor with a balloon in balloon.
+	public void TeddyBearYC() {
+		Camera camera = new Camera(new Point3D(0, 0, 1700), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+				.setViewPlaneSize(150, 150).setDistance(1000);	
 
-		scene.geometries.add( //
-				new Sphere( new Point3D(0, 0, -1000),200).setEmmission(new Color(100,0,0)) .setMaterial( new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(1).setkR(0)));
-					 
-						 
-				new Sphere( new Point3D(-300, 0, -1200),150).setEmmission(new Color(0,0,100)).setMaterial( new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0).setkR(0));
+		scene.geometries.add(
+				new Sphere( new Point3D(-40, 70, -120), 20) // the center sphere-  left ear
+				.setEmmission(new Color(185,124,90)).setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0.3).setkR(0)),
+				new Sphere( new Point3D(40, 70, -120), 20) // the center sphere-right ear
+				.setEmmission(new Color(185,124,90)).setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0.3).setkR(0)),
+	
+				new Sphere( new Point3D(-38, 68, -120), 13) // the center sphere-  left ear inner
+				.setEmmission(new Color(255,0,128)).setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0.3).setkR(0)),
+				new Sphere( new Point3D(38, 68, -120), 13) // the center sphere-right ear inner
+				.setEmmission(new Color(255,0,128)).setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0.3).setkR(0)),
+	
+				new Sphere( new Point3D(0, 20, -100), 15) // the center sphere-  mouth
+				.setEmmission(new Color(255,0,128)).setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0.3).setkR(0)),
+				
+				new Sphere( new Point3D(16, 42,-65),7) //right eye
+						.setEmmission(new Color(69,43,29)),
+				new Sphere( new Point3D(-16, 42, -65),7) //left eye
+						.setEmmission(new Color(69,43,29)),
+				new Triangle(new Point3D(-3, 25, 95), new Point3D(3, 25, 115), new Point3D(0,19, 130)).setEmmission(new Color(69,43,29)).setMaterial(new Material().setkD(0.5).setkS(0.5).setnShininess(30)), //triangle of nose
+				new Sphere( new Point3D(0, 40, -100), 40) // the center sphere-head
+						.setEmmission(new Color(185,124,90)).setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0.3).setkR(0)),
+			
+						new Sphere( new Point3D(0,-50, -120), 60) // the center body
+						.setEmmission(new Color(185,124,90)).setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0.3).setkR(0)),
+			
+						new Sphere( new Point3D(-50, -13, -100), 23) // the center sphere-  left hand
+						.setEmmission(new Color(185,124,90)).setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0.3).setkR(0)),
+						new Sphere( new Point3D(50, -13, -100), 23) // the center sphere-right hand
+						.setEmmission(new Color(185,124,90)).setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0.3).setkR(0)),
 				
 						
-				new Sphere(new Point3D(-210, 110, -900),60).setEmmission(new Color(25,25,25)).setMaterial( new Material().setkD(0).setkS(0.2).setnShininess(1000).setkT(0.6).setkR(0.2));
-				
-				//  NEW
-				new Sphere( new Point3D(0, 0, -500),200).setEmmission(new Color(255,128,192)) .setMaterial( new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(1).setkR(0));
-				 
-				 
-				new Sphere( new Point3D(300, 0, 1200),150).setEmmission(new Color(255,128,192)).setMaterial( new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0).setkR(0));
-				
+						new Sphere( new Point3D(-40, -100, -100), 30) // the center sphere-  left leg
+						.setEmmission(new Color(185,124,90)).setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0.3).setkR(0)),
+						new Sphere( new Point3D(40, -100, -100), 30) // the center sphere-right leg
+						.setEmmission(new Color(185,124,90)).setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0.3).setkR(0)),
+			
+						new Sphere( new Point3D(-95, 70, -120),36) // the center sphere- baloon
+						.setEmmission(new Color(30,0,50)) //
+						.setMaterial(new Material().setkD(0).setkS(0.2).setnShininess(1000).setkT(0.6).setkR(0.2)),
+						new Sphere( new Point3D(-95, 70, -120),20) // the center sphere- baloon
+						 .setEmmission(new Color(0,100,100)).setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0).setkR(1)),
 						
-				new Sphere(new Point3D(210, -110, 900),60).setEmmission(new Color(255,128,192)).setMaterial( new Material().setkD(0).setkS(0.2).setnShininess(1000).setkT(0.6).setkR(0.2));
+						 new Triangle(new Point3D(-70, 42, 95), new Point3D(-67, 42, 115), new Point3D(-50,-5, 130)).setEmmission(new Color(java.awt.Color.RED)).setMaterial(new Material().setkD(0.5).setkS(0.5).setnShininess(30)), //triangle of nose
+						
+						 new Sphere( new Point3D(-15, -40, -65), 18) // the center sphere-  left heart
+							.setEmmission(new Color(java.awt.Color.RED)),
+							new Sphere( new Point3D(15, -40, -65), 18) // the center sphere-right heart
+							.setEmmission(new Color(java.awt.Color.RED)),
+							 new Triangle(new Point3D(-28.7,-44.5, 95), new Point3D(28.6, -44.5, 115), new Point3D(0, -63, 130)).setEmmission(new Color(java.awt.Color.RED)),//.setMaterial(new Material().setkD(0.25).setkS(0.25).setnShininess(20).setkT(0).setkR(0.8)),//.setMaterial(new Material().setkD(0.5).setkS(0.5).setnShininess(30)), //triangle of nose	
+						
+				new Triangle(new Point3D(-150, -150, -115), new Point3D(150, -150, -115), new Point3D(150, 0, -150)) //floor
+		.setEmmission(new Color(125, 158, 209)) //
+		.setMaterial(new Material().setkR(0.2)), //
+new Triangle(new Point3D(-150, -150, -115), new Point3D(-150, -5, -150), new Point3D(150, 0, -150)) //floor
+.setEmmission(new Color(125, 158, 209)) //
+.setMaterial(new Material().setkR(0.2)));
+ //
+
 		
-		scene.lights.add(
-				new SpotLight(new Color(200, 200, 200), new Point3D(-600, -200, -550), 0.1, 0.00001, 0.000003, new Vector(2, 2, -3)));
+		scene.lights.add( //
+				new SpotLight(new Color(1000, 600, 400), new Point3D(-250, 400, 1500),1 ,0.0004,0.0000006,  new Vector(-1, -1, -2)) //
+						.setkL(0.0004).setkQ(0.0000006));
+		scene.lights.add(new DirectionalLight(new Color(200,100,0), new Vector(-1, -1, -1))); //add directional light
 		
-			/*	scene.lights.add(new SpotLight(new Color(200,200,200), new Point3D(1000,-550,1000), 1, 4E-5, 2E-7,  new Vector(0,-1,0)) //
-						.setkL(4E-5).setkQ(2E-7));
-				scene.lights.add(new PointLight(new Color(300, 500, 500), new Point3D(-50, -50, 50), 1, 0.00001, 0.000001)//
-						.setkL(0.00001).setkQ(0.000001));*/
-				
-		ImageWriter imageWriter = new ImageWriter("threeSpheresReflectionRefraction", 600, 600);
-				
-		//ImageWriter imageWriter = new ImageWriter("three spheres reflection/refraction", 600, 600);
-		Render render =new Render() //
-				.setImageWriter(imageWriter) //
+		
+		Render render = new Render() //
+				.setImageWriter(new ImageWriter("TeddyBearY&C", 500, 500)) //
 				.setCamera(camera) //
 				.setRayTracer(new RayTracerBasic(scene));
 		render.renderImage();
 		render.writeToImage();
 	}
 	
-	/*
-	@Test
-	public void ourPicture() {
-		Camera camera = new Camera(new Point3D(0, 0,0), new Vector(0, 0, -1), new Vector(0, -1, 0)) //
-				.setViewPlaneSize(300, 300).setDistance(400);
-
-		scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
+	@Test//c&y test with all new affect
+	public void allNewEffect() {
+		Camera camera = new Camera(new Point3D(0, 0, 1500), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+				.setViewPlaneSize(150, 150).setDistance(1000);
 
 		scene.geometries.add( //
-				new Sphere(new Point3D(0,50,-800),120) //the turquise sphere
-				.setEmmission(new Color(0,100,100)) //
-				.setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0).setkR(1)),
-				new Sphere(new Point3D(-210,110,-900),300) //the large purple sphere
-				.setEmmission(new Color(30,0,50)) //
-				.setMaterial(new Material().setkD(0).setkS(0.2).setnShininess(1000).setkT(0.6).setkR(0.2)),
-				new Sphere(new Point3D(300,50,-200),80) //the blue sphere (not seen boldly)
-				.setEmmission(new Color(0,0,100)) //
-				.setMaterial(new Material().setkD(0.6).setkS(0.9).setnShininess(1000).setkT(0).setkR(0.2)),
-				new Sphere(new Point3D(10, 30, -100),10) //the red sphere
-				.setEmmission(new Color(java.awt.Color.RED)) //
-				.setMaterial(new Material().setkD(0.25).setkS(0.25).setnShininess(20).setkT(0).setkR(0.8)),
-				new Triangle(new Point3D(1500, -1500, -1500), new Point3D(-1500, 1500, -1500),//the triangle
-				new Point3D(-1500, -1500, -2000))
-				.setMaterial(new Material().setkD(0.25).setkS(0.9).setnShininess(20).setkT(0).setkR(1))); 
-
-
-		scene.lights.add(new SpotLight(new Color(200,200,200), new Point3D(1000,-550,1000), 1, 4E-5, 2E-7,  new Vector(0,-1,0)) //
-				.setkL(4E-5).setkQ(2E-7));
-		scene.lights.add(new PointLight(new Color(300, 500, 500), new Point3D(-50, -50, 50), 1, 0.00001, 0.000001)//
-				.setkL(0.00001).setkQ(0.000001));
+				new Triangle(new Point3D(-150, -150, -115), new Point3D(150, -150, -135), new Point3D(75, 75, -150)) //
+				.setEmmission(new Color(java.awt.Color.BLACK)) .setMaterial(new Material().setkD(0.5).setkS(0.5).setnShininess(60).setkT(1)), //
+		new Triangle(new Point3D(-150, -150, -115), new Point3D(-70, 70, -140), new Point3D(75, 75, -150)) //
+				.setMaterial(new Material().setkD(0.01).setkS(0.5).setnShininess(60).setkR(1)), //
 		
-		ImageWriter imageWriter = new ImageWriter("ourPictureRefrectionRefraction", 600, 600);
+				new Sphere( new Point3D(0, 0, -50), 50) //
+						.setEmmission(new Color(java.awt.Color.BLUE)) //
+						.setMaterial(new Material().setkD(0.4).setkS(0.3).setnShininess(100).setkT(0.6)),
+				new Sphere( new Point3D(0, 0, -50), 25) //
+						.setEmmission(new Color(java.awt.Color.RED)) );//
+		scene.lights.add(new DirectionalLight(new Color(100,100,100), new Vector(0, 0, -1)));
+		scene.lights.add(new SpotLight(new Color(430,230,230), new Point3D(60, 50, 0),1, 4E-5,2E-7,  new Vector(0, 0, -1)) //
+				.setkL(4E-5).setkQ(2E-7));
+		
 		Render render = new Render() //
-				.setImageWriter(imageWriter) //
+				.setImageWriter(new ImageWriter("allNewEffect", 600,600)) //
 				.setCamera(camera) //
 				.setRayTracer(new RayTracerBasic(scene));
-
 		render.renderImage();
 		render.writeToImage();
-	}*/
+	}
+
 	
 
-
+	
+		
 }
